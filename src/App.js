176a32dcc66f257901580fa6f737df1dcadd1321
Todd Whitelow <todd@whitelow.tech>
2020-11-27 17:23:20 -0700

    create components out of template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Searchbar from './components/Searchbar'
+import { Component } from 'react';
+import Navigation from './components/Navigation';
+import Results from './components/Results';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div class="container">
+          <Searchbar />
+          <Navigation />
+          <Results />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,21 @@
+import { Component } from 'react';
+
+class Navigation extends Component {
+  render() {
+    return (
+      <>
+        <nav class="main-nav">
+          <ul>
+            <li><a href='#'>Cats</a></li>
+            <li><a href='#'>Dogs</a></li>
+            <li><a href='#'>Computers</a></li>
+          </ul>
+        </nav>
+      </>
+    );
+  }
+}
+
+export default Navigation;
+
+
