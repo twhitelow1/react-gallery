6d8ecc401b5ec161712b67ab94f1820a0aa042bc
Todd Whitelow <todd@whitelow.tech>
2020-11-27 23:44:28 -0700

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Header from './components/Header'
 import { Component } from 'react';
 import apiKey from './config';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Potter from './components/Topics/Potter';
 import Marvel from './components/Topics/Marvel';
 import StarWars from './components/Topics/StarWars';
 import The100 from './components/Topics/The100';
+import NotFound from './components/NotFound';
 
 class App extends Component {
   render() {
@@ -16,11 +17,14 @@ class App extends Component {
         <BrowserRouter>
           <div className="container">
             <Header />
-            <Route exact path="/" render={() => <Home />} />
-            <Route path="/potter" render={() => <Potter />} />
-            <Route path="/marvel" render={() => <Marvel />} />
-            <Route path="/starwars" render={() => <StarWars />} />
-            <Route path="/the100" render={() => <The100 />} />
+            <Switch>
+              <Route exact path="/" render={() => <Home />} />
+              <Route path="/potter" render={() => <Potter title='Potter' />} />
+              <Route path="/marvel" render={() => <Marvel />} />
+              <Route path="/starwars" render={() => <StarWars />} />
+              <Route path="/the100" render={() => <The100 />} />
+              <Route component={NotFound} />
+            </Switch>
           </div>
         </BrowserRouter>
       </>
