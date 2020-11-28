a25be3c590637e6630b998f480dcef6a0f3621de
Todd Whitelow <todd@whitelow.tech>
2020-11-27 17:40:04 -0700

    add photos array

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -7,6 +7,7 @@ class Photo extends Component {
         <li>
           <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="" />
         </li>
+
       </>
     );
   }
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,4 +1,12 @@
 import { Component } from 'react';
+import Photo from './Photo';
+
+const photos = [
+  "https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg",
+  "https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg",
+  "https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg",
+  "https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg",
+]
 
 class Results extends Component {
   render() {
@@ -7,8 +15,7 @@ class Results extends Component {
         <div class="photo-container">
           <h2>Results</h2>
           <ul>
-
-
+            <Photo />
           </ul>
         </div>
       </>
